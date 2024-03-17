c436cd6ce939061faac55a249bc3cbb117e6f688
Radekor500 <radek00500@gmail.com>
2024-03-17 22:06:12 +0100

    sample endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use http_server::{HttpServer, router::Router};
+use http_server::{router::{HttpResponse, Router}, HttpServer};
 
 fn main() {
     let server = HttpServer {
@@ -8,11 +8,11 @@ fn main() {
     let mut router = Router::new();
     router.add_route("/","GET", |data| {
         println!("Request to root path with data {}",data);
-        Ok(())
+        Ok(HttpResponse::new("Hello from root path".to_string(), None, 200))
     });
-    router.add_route("/other", "GET", |data| {
+    router.add_route("/error", "GET", |data| {
         println!("Request to other path with data {}",data);
-        Ok(())
+        Ok(HttpResponse::new("Error occured".to_string(), None, 500))
     });
     server.run(router).expect("Starting server failed");
 }
