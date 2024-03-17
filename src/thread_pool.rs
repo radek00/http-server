02fff9923a107487d29d5af30d818733ab3d8ce2
Radekor500 <radek00500@gmail.com>
2024-03-17 14:12:54 +0100

    thread pool implementation

---
+++ src/thread_pool.rs
@@ -0,0 +1,88 @@
+use std::{fmt, sync::{mpsc, Arc, Mutex}, thread};
+
+#[derive(Debug)]
+pub struct PoolCreationError;
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed to create thread pool: size must be greater than 0")
+    }
+}
+
+impl std::error::Error for PoolCreationError {}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+
+}
+
+impl ThreadPool {
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size <= 0{
+            return Err(PoolCreationError)
+        }
+        let mut workers = Vec::with_capacity(size);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver  = Arc::new(Mutex::new(receiver));
+
+        for thread_id in 0..size {
+            workers.push(Worker::new(thread_id, Arc::clone(&receiver)));
+        }
+
+        Ok(ThreadPool { workers, sender: Some(sender) })
+    }
+
+    pub fn execute<F>(&self, f: F) -> Result<(), mpsc::SendError<Job>>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.as_ref().unwrap().send(job)?;
+        Ok(())
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker { id, thread: Some(thread) }
+    }
+    
+}
