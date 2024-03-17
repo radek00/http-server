066c936aa2017b81ebd85977e0b4e426ab46d611
Radekor500 <radek00500@gmail.com>
2024-03-17 22:05:45 +0100

    return https responses from router

--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,22 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Error};
 
-type Handler = Box<dyn Fn(&str) -> Result<(), Box<dyn std::error::Error + Send + 'static>> + Send + Sync>;
+pub struct HttpResponse {
+    pub content_type: String,
+    pub body: String,
+    pub status_code: u16,
+}
+
+impl HttpResponse {
+    pub fn new(body: String, content_type: Option<String>, status_code: u16) -> Self {
+        HttpResponse {
+            content_type: content_type.unwrap_or_else(|| "application/json".to_string()),
+            body,
+            status_code
+        }
+    }
+}
+
+type Handler = Box<dyn Fn(&str) -> Result<HttpResponse, Error> + Send + Sync>;
 
 pub struct Router {
     routes: HashMap<String, Handler>,
@@ -15,17 +31,25 @@ impl Router {
 
     pub fn add_route<F>(&mut self, path: &str, method: &str, handler: F)
     where
-        F: Fn(&str) -> Result<(), Box<dyn std::error::Error + Send + 'static>> + Send + Sync + 'static,
+        F: Fn(&str) -> Result<HttpResponse, Error> + Send + Sync + 'static,
     {
         self.routes.insert(format!("{}{}", method, path), Box::new(handler));
     }
 
-    pub fn route(&self, path: &str, method: &str, data: &str) -> Result<(), Box<dyn std::error::Error + Send + 'static>> {
+    pub fn route(&self, path: &str, method: &str, data: &str) -> Result<HttpResponse, Box<dyn std::error::Error>> {
         match self.routes.get(&format!("{}{}", method, path)) {
-            Some(handler) => handler(data),
+            Some(handler) => {
+                let response = handler(data)?;
+                println!("Response: {:?}", response.body);
+                Ok(response)
+            },
             None => {
                 println!("No route found for path: {}", path);
-                Ok(())
+                Ok(HttpResponse::new(
+                    format!("No route found for path: {}", path),
+                    None,
+                    404,
+                ))
             },
         }
     }
