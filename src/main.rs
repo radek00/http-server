97162c01f8c52e343c23609316d96e2c895ea669
Radekor500 <radek00500@gmail.com>
2024-03-24 16:30:33 +0100

    logic for retriving file and directory list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use http_server::{router::{Body, HttpResponse, Router}, HttpServer};
 use serde::{Deserialize, Serialize};
 
@@ -5,6 +7,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 struct Example {
     message: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Files {
+    path: String,
+    file_type: String,
 }
 
 fn main() {
@@ -27,9 +35,32 @@ fn main() {
     
     });
 
+    router.add_route("/directory", "GET", |_, params| {
+        Ok(HttpResponse::new(Body::Json(list_directory(params.unwrap().get("path").unwrap())), None, 200))
+    });
+
     router.add_route("/json", "GET", |_, _ | {
         Ok(HttpResponse::new(Body::Json(serde_json::to_value(Example {message: String::from("Hello")}).unwrap()), None, 200))
     
     });
     server.run(router).expect("Starting server failed");
+}
+
+
+fn list_directory(path: &str) -> serde_json::Value {
+    let paths = fs::read_dir(path).unwrap();
+
+    let mut path_info = Vec::new();
+    for path in paths {
+        let path = path.unwrap();
+        let file = Files {
+            path: format!("{}", path.path().display()),
+            file_type: if path.path().is_dir() { "directory".to_string() } else { "file".to_string() },
+        };
+        path_info.push(file);
+    }
+
+    let v = serde_json::to_value(&path_info).unwrap();
+    
+    v
 }
