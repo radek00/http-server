b5dd611749a39d59b9b9aed5330773900e30d28e
Radekor500 <radek00500@gmail.com>
2024-03-30 15:53:02 +0100

    remove options from HashMap and build directory path based on curent path

--- src/main.rs
+++ src/main.rs
@@ -29,31 +29,33 @@ fn main() {
     let mut router = Router::new();
 
     router.add_route("/{file}?","GET", |_, params| {
-        let file_name = match params {
-            Some(p) => p.get("file").unwrap(),
+        let file_name = match params.get("file") {
+            Some(file) => file,
             None => "index.html",
+            
         };
+
         let contents = fs::read_to_string(file_name)?;
-        Ok(HttpResponse::new(Body::Text(contents), Some(String::from("text/html")), 200))
+        Ok(HttpResponse::new(Body::Text(contents), Some(mime_guess::from_path(file_name).first_or_text_plain().to_string()), 200))
     });
     router.add_route("/api/error", "GET", |data, _| {
         println!("Request to other path with data {}",data.unwrap());
         Ok(HttpResponse::new(Body::Text("Error occured".to_string()),Some(String::from("text/plain")), 500))
     });
     router.add_route("/api/files", "GET", |_, params| {
-        let file_name = params.unwrap().get("path").unwrap();
-        let file = File::open(file_name)?;
+        let file_name = format!("./{}", params.get("path").ok_or("Missing required query param")?);
+        let file = File::open(&file_name)?;
         Ok(HttpResponse::new(Body::File(file), Some(mime_guess::from_path(file_name).first_or_octet_stream().to_string()), 200))
     
     });
 
     router.add_route("/api/directory", "GET", |_, params| {
-        println!("Request to directory path with query param: {:?}", params.unwrap());
-        Ok(HttpResponse::new(Body::Json(list_directory(params.unwrap().get("path").unwrap())?), None, 200))
+        //println!("Request to directory path with query param: {:?}", params.unwrap());
+        Ok(HttpResponse::new(Body::Json(list_directory(&format!("./{}", params.get("path").unwrap_or_else(|| {&""})))?), None, 200))
     });
 
     router.add_route("/api/json", "GET", |_, _ | {
-        Ok(HttpResponse::new(Body::Json(serde_json::to_value(Example {message: String::from("Hello")}).unwrap()), None, 200))
+        Ok(HttpResponse::new(Body::Json(serde_json::to_value(Example {message: String::from("Hello")})?), None, 200))
     
     });
     server.run(router).expect("Starting server failed");
