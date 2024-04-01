0631a84297ceabf7c365345aad22bc7da127f29d
Radekor500 <radek00500@gmail.com>
2024-04-01 15:05:45 +0200

    rendering file tree

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
-function fetchFiles() {
-    fetch('/api/directory?path=/')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        });
+let previousPath = '';
+
+function renderUpButton() {
+    const button  = document.getElementById('up-button');
+    button.onclick = () => {
+        renderFileTree(previousPath);
+    }
+    button.innerText = `UP - ${previousPath}`;
 }
-fetchFiles();
+
+async function fetchFiles(path = "") {
+    const files = await fetch(`/api/directory?path=${path}`);
+    return await files.json();
+}
+
+async function renderFileTree(path = "") {
+    const tbody = document.querySelector('tbody');
+    tbody.innerHTML = '';
+    const files = await fetchFiles(path);
+
+    files.forEach(file => {
+        const tr = document.createElement('tr');
+        const name = document.createElement('td');
+        const fileLink = document.createElement('a');
+        if (file.file_type === 'Directory') {
+            fileLink.href = '#';
+            fileLink.textContent = `${file.name}/`;
+            fileLink.onclick = (event) => {
+                event.preventDefault();
+                previousPath = `${file.path.split('/').slice(0, -1).join('/')}/`;
+                renderUpButton();
+                renderFileTree(file.path);
+            }
+        } else {
+            fileLink.href = `api/files?path=${file.path}`;
+            fileLink.textContent = file.name;
+        }
+        name.appendChild(fileLink);
+        const lastModified = document.createElement('td');
+        const size = document.createElement('td');
+        lastModified.textContent = file.last_modified;
+        size.textContent = file.size;
+        tr.appendChild(name);
+        tr.appendChild(lastModified);
+        tr.appendChild(size);
+        tbody.appendChild(tr);
+    });
+};
+
+renderUpButton();
+renderFileTree(previousPath);
