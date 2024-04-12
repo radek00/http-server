b1d63ba3dd8a9a20cc87a2560133457a98215901
Radekor500 <radek00500@gmail.com>
2024-04-12 13:52:15 +0200

    use static lifetime for StaticFiles to prevent cloning

--- src/static_files.rs
+++ src/static_files.rs
@@ -1,21 +1,21 @@
 use std::collections::HashMap;
 
 pub struct StaticFiles {
-    pub content: HashMap<String, Vec<u8>>,
+    pub content: HashMap<String, &'static [u8]>,
 }
 
 impl StaticFiles {
     pub fn new() -> Self {
-        let mut content = HashMap::new();
-        content.insert(String::from("script.js"), include_bytes!("./dist/script.js").to_vec());
-        content.insert(String::from("index.html"), include_bytes!("./dist/index.html").to_vec());
+        let mut content: HashMap<String, &'static [u8]> = HashMap::new();
+        content.insert(String::from("script.js"), include_bytes!("./dist/script.js"));
+        content.insert(String::from("index.html"), include_bytes!("./dist/index.html"));
         
         StaticFiles {
             content,
         }
     }
 
-    pub fn get(&self, file_name: &str) -> Result<&Vec<u8>, Box<dyn std::error::Error>> {
+    pub fn get(&self, file_name: &str) -> Result<&'static [u8], Box<dyn std::error::Error>> {
         let file = self.content.get(file_name).ok_or("File not found")?;
         Ok(file)
     }
