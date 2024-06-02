2ccf982d1c5527496eb08fc77e23a7399ffc0ab4
Radekor500 <radek00500@gmail.com>
2024-06-02 13:35:56 +0300

    separate module for all errors

---
+++ src/errors/http_parse_error.rs
@@ -0,0 +1,22 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct HttpParseError {
+    pub message: String,
+}
+
+impl fmt::Display for HttpParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl std::error::Error for HttpParseError {}
+
+impl From<std::io::Error> for HttpParseError {
+    fn from(error: std::io::Error) -> Self {
+        HttpParseError {
+            message: error.to_string(),
+        }
+    }
+}
