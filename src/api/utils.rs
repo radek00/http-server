6e65763fce463295d6b43731dd621f44bb959d8f
Radekor500 <radek00500@gmail.com>
2024-06-07 16:39:15 +0300

    allow to only fetch directories relative to current root

--- src/api/utils.rs
+++ src/api/utils.rs
@@ -31,6 +31,12 @@ struct Files {
     size: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct DirectoryInfoResponse {
+    paths: Vec<PathParts>,
+    files: Vec<Files>,
+}
+
 fn human_bytes<T: Into<f64>>(bytes: T) -> String {
     let size = bytes.into();
 
@@ -77,17 +83,40 @@ pub fn split_path(path: &str) -> Result<serde_json::Value, ApiError> {
 }
 
 pub fn list_directory(path: &str) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
-    let path = PathBuf::from(path).canonicalize()?;
-    let paths = fs::read_dir(path)?;
+    let cannonical_root_path = Path::new("./").canonicalize()?;
+    let root_path = PathBuf::from("./").join(path);
 
-    let mut path_info = Vec::new();
-    for path in paths {
+    let mut current_full_path = String::new();
+    let mut directory_response = DirectoryInfoResponse {
+        paths: root_path
+            .components()
+            .map(|c| {
+                let part_name = c.as_os_str().to_string_lossy().to_string();
+                current_full_path.push_str(&part_name);
+                current_full_path.push('/');
+                //let full_path = format!("{}/{}", current_full_path, part_name);
+                PathParts {
+                    part_name,
+                    full_path: current_full_path.clone(),
+                }
+            })
+            .collect(),
+        files: Vec::new(),
+    };
+
+    let directory_contents = fs::read_dir(root_path.canonicalize()?)?;
+
+    for path in directory_contents {
         let path = path?;
         let system_time: DateTime<Utc> = path.metadata()?.modified()?.into();
 
         let file = Files {
             name: path.file_name().into_string().unwrap(),
-            path: path.path().to_string_lossy().into_owned(),
+            path: path
+                .path()
+                .strip_prefix(&cannonical_root_path)?
+                .to_string_lossy()
+                .into_owned(),
             file_type: if path.path().is_dir() {
                 FileType::Directory
             } else {
@@ -96,10 +125,10 @@ pub fn list_directory(path: &str) -> Result<serde_json::Value, Box<dyn std::erro
             last_modified: system_time.format("%d/%m/%Y %T").to_string(),
             size: human_bytes(path.metadata()?.len() as f64),
         };
-        path_info.push(file);
+        directory_response.files.push(file);
     }
 
-    let v = serde_json::to_value(path_info)?;
+    let v = serde_json::to_value(directory_response)?;
 
     Ok(v)
 }
