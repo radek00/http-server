7624a0ebb59508a68797d895a87e6c664409ac62
Radekor500 <radek00500@gmail.com>
2024-06-29 14:11:55 +0300

    move clap build from library to binary

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,9 +1,58 @@
-use scratch_server::{api_error::ApiError, Body, HttpMethod, HttpResponse, Router, STATIC_FILES};
+use scratch_server::{
+    api_error::ApiError, Body, HttpMethod, HttpResponse, HttpServer, Router, STATIC_FILES,
+};
 use std::{fs::File, path::PathBuf};
 
 use self::utils::list_directory;
 
 mod utils;
+
+pub fn build_server() -> HttpServer {
+    let mut args = clap::Command::new("Simple HTTP Server")
+            .version("1.0")
+            .author("radek00")
+            .about("Simlpe HTTP Server with TLS/SSL support. Implemented api endpoints allow for navigating file system directories, uploading and downloading files.")
+            .arg(clap::Arg::new("port")
+                .short('p')
+                .value_parser(clap::value_parser!(u16))
+                .default_value("7878")
+                .long("port")
+                .help("Sets the port number"))
+            .arg(clap::Arg::new("threads")
+                .short('t')
+                .value_parser(clap::value_parser!(usize))
+                .default_value("12")
+                .long("threads")
+                .help("Sets the number of threads"))
+            .arg(clap::Arg::new("cert")
+                .short('c')
+                .value_parser(clap::value_parser!(PathBuf))
+                .required(false)
+                .long("cert")
+                .help("TLS/SSL certificate"))
+            .arg(clap::Arg::new("certpass")
+                .long("certpass")
+                .default_value("")
+                .help("TLS/SSL certificate password"))
+            .arg(clap::Arg::new("silent")
+                .action(clap::ArgAction::SetTrue)
+                .short('s')
+                .long("silent")
+                .help("Disable logging"))
+            .get_matches();
+
+    let mut server = HttpServer::build(
+        args.remove_one::<u16>("port").unwrap(),
+        args.remove_one::<usize>("threads").unwrap(),
+        args.remove_one::<PathBuf>("cert"),
+        args.remove_one::<String>("certpass"),
+    );
+
+    if !args.get_flag("silent") {
+        server = server.with_logger();
+    }
+    server
+}
 
 pub fn create_routes() -> Box<dyn Fn(&mut Router) + Send + Sync> {
     let closure = |router: &mut Router| {
