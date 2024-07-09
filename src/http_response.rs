434bf48d684543f6dfc263c01153411ca57bb46b
Radekor500 <radek00500@gmail.com>
2024-07-09 22:15:25 +0300

    basic compression handling

--- src/http_response.rs
+++ src/http_response.rs
@@ -1,7 +1,10 @@
 use std::{
     fs::File,
-    io::{self, BufReader},
+    io::{self, BufReader, Write},
 };
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
 
 use crate::ReadWrite;
 
@@ -43,43 +46,97 @@ impl HttpResponse {
             self.status_code, self.content_type
         );
 
+            //check if compression is enabled
+    //check if accept-encoding header is present
+    //if gzip is supported, compress the response
+    //if not, return the response as is
+    //set the content-encoding header to gzip
+    //set Vary header to Accept-Encoding
+
         self.headers.iter().for_each(|(key, value)| {
             base_headers.push_str(&format!("{}: {}\r\n", key, value));
         });
 
         if let Some(body) = self.body {
-            let body = match body {
-                Body::Text(text) => text.clone(),
-                Body::Json(json) => json.to_string(),
-                Body::StaticFile(file, _) => {
-                    base_headers.push_str(&format!("Content-Length: {}\r\n", file.len()));
-                    base_headers.push_str("\r\n");
-                    stream.write_all(base_headers.as_bytes())?;
-                    stream.write_all(file)?;
-                    return Ok(());
-                }
-                Body::FileStream(file, name) => {
-                    base_headers.push_str(&format!(
-                        "Content-Disposition: attachment; filename=\"{}\"\r\n\
-                    \rn",
-                        name
-                    ));
-                    stream.write_all(base_headers.as_bytes())?;
-                    let mut reader = BufReader::new(file);
-                    io::copy(&mut reader, &mut stream)?;
-                    return Ok(());
-                }
-            };
-            base_headers.push_str(&format!(
-                "Content-Length: {}\r\n\
-            \r\n\
-            {}",
-                body.len(),
-                body
-            ));
-        }
+            if true {
+                //if compression is enabled
+                base_headers.push_str("Content-Encoding: gzip\r\n");
+                base_headers.push_str("Vary: Accept-Encoding\r\n");
+    
+                let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
 
-        stream.write_all(base_headers.as_bytes())?;
+                match body {
+                    Body::Text(text) => {
+                        encoder.write_all(text.as_bytes())?
+                    },
+                    Body::Json(json) => {
+                        encoder.write_all(json.to_string().as_bytes())?
+                    },
+                    Body::StaticFile(file, _) => {
+                        encoder.write_all(file)?;
+                        let encoded = encoder.finish()?;
+                        base_headers.push_str(&format!("Content-Length: {}\r\n", encoded.len()));
+                        base_headers.push_str("\r\n");
+                        stream.write_all(base_headers.as_bytes())?;
+                        stream.write_all(&encoded)?;
+                        return Ok(());
+                    }
+                    Body::FileStream(file, name) => {
+                        base_headers.push_str(&format!(
+                            "Content-Disposition: attachment; filename=\"{}\"\r\n\
+                        \rn",
+                            name
+                        ));
+                        stream.write_all(base_headers.as_bytes())?;
+                        let mut reader = BufReader::new(file);
+                        io::copy(&mut reader, &mut stream)?;
+                        return Ok(());
+                    }
+                }
+
+                let encoded = encoder.finish()?;
+                base_headers.push_str(&format!(
+                    "Content-Length: {}\r\n\
+                \r\n\
+                ",
+                    encoded.len(),
+                ));
+                stream.write_all(base_headers.as_bytes())?;
+                stream.write_all(&encoded)?;
+            } else {
+                let body = match body {
+                    Body::Text(text) => text.clone(),
+                    Body::Json(json) => json.to_string(),
+                    Body::StaticFile(file, _) => {
+                        base_headers.push_str(&format!("Content-Length: {}\r\n", file.len()));
+                        base_headers.push_str("\r\n");
+                        stream.write_all(base_headers.as_bytes())?;
+                        stream.write_all(file)?;
+                        return Ok(());
+                    }
+                    Body::FileStream(file, name) => {
+                        base_headers.push_str(&format!(
+                            "Content-Disposition: attachment; filename=\"{}\"\r\n\
+                        \rn",
+                            name
+                        ));
+                        stream.write_all(base_headers.as_bytes())?;
+                        let mut reader = BufReader::new(file);
+                        io::copy(&mut reader, &mut stream)?;
+                        return Ok(());
+                    }
+                };
+                base_headers.push_str(&format!(
+                    "Content-Length: {}\r\n\
+                \r\n\
+                {}",
+                    body.len(),
+                    body
+                ));
+                stream.write_all(base_headers.as_bytes())?;
+            }
+    
+            }
 
         Ok(())
     }
