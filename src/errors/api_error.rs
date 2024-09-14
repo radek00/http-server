49467ad3ede17ca3c21e37d4b05acfa6aeca4549
Radekor500 <radek00500@gmail.com>
2024-09-14 15:20:17 +0300

    change error code

--- src/errors/api_error.rs
+++ src/errors/api_error.rs
@@ -49,7 +49,7 @@ impl std::error::Error for ApiError {}
 
 impl From<std::io::Error> for ApiError {
     fn from(error: std::io::Error) -> Self {
-        ApiError::new_with_html(404, &format!("IO Error: {}", error))
+        ApiError::new_with_html(500, &format!("IO Error: {}", error))
     }
 }
 
